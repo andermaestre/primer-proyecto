4b63dcb02bcd28571ada200f41fd2f3027aacbf1
105maura <ander.maestre@inetum.com>
2024-05-31 13:26:40 +0200

    Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HolaMundoComponent } from './components/hola-mundo/hola-mundo.component';
+import { FormularioEjemploComponent } from './components/formulario-ejemplo/formulario-ejemplo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/hola-mundo', pathMatch: 'full' },
+  { path: 'hola-mundo', component: HolaMundoComponent },
+  { path: 'formulario', component: FormularioEjemploComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
